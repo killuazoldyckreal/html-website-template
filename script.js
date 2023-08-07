3bc8bdaa2b7913e9339aa4c46921f57bfb16e7a4
Killua_Zoldyck <58257876+killuazoldyckreal@users.noreply.github.com>
2023-08-07 13:04:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
-      // Select all anchor links starting with '#'
-      const anchorLinks = document.querySelectorAll('a[href^="#"]');
+  // Select all anchor links starting with '#'
+  const anchorLinks = document.querySelectorAll('a[href^="#"]');
+  
+  // Attach click event listener to each anchor link
+  anchorLinks.forEach(link => {
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
       
-      // Attach click event listener to each anchor link
-      anchorLinks.forEach(link => {
-        link.addEventListener('click', function(e) {
-          e.preventDefault();
-          
-          // Get the target element's ID from the link's href attribute
-          const targetId = this.getAttribute('href').substring(1);
-          
-          // Scroll smoothly to the target element
-          document.getElementById(targetId).scrollIntoView({
-            behavior: 'smooth'
-          });
-        });
+      // Get the target element's ID from the link's href attribute
+      const targetId = this.getAttribute('href').substring(1);
+      
+      // Scroll smoothly to the target element
+      document.getElementById(targetId).scrollIntoView({
+        behavior: 'smooth'
       });
     });
+  });
+
+  // Toggle mobile menu
+  document.querySelector('.menu-icon').addEventListener('click', function() {
+    document.querySelector('.nav-links').classList.toggle('active');
+  });
+});
