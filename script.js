268970098a7162c44bd5d3a16872f8f55951a09c
Killua_Zoldyck <58257876+killuazoldyckreal@users.noreply.github.com>
2023-08-07 10:50:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+      // Select all anchor links starting with '#'
+      const anchorLinks = document.querySelectorAll('a[href^="#"]');
+      
+      // Attach click event listener to each anchor link
+      anchorLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+          e.preventDefault();
+          
+          // Get the target element's ID from the link's href attribute
+          const targetId = this.getAttribute('href').substring(1);
+          
+          // Scroll smoothly to the target element
+          document.getElementById(targetId).scrollIntoView({
+            behavior: 'smooth'
+          });
+        });
+      });
+    });
